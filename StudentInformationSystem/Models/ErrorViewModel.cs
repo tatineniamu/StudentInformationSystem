0c8d2290a78ea8df76eda2de044dc7dec57bce01
Karthik Polina <karthik.p@dsiginc.com>
2022-02-06 20:05:54 -0500

    Add project files.

---
+++ StudentInformationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentInformationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
